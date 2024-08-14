6c388be0cf6d7dfa2e18f526b87f58beadc53be6
Francisco Soto <panchon.ues@gmail.com>
2024-08-13 17:50:50 -0700

    first commit

---
+++ src/presentation/navigator/StackNavigator.tsx
@@ -0,0 +1,37 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { Screen1 } from '../screens/Screen1/Screen1';
+import { Screen2 } from '../screens/Screen2/Screen2';
+
+
+
+
+
+
+export type RootStackParams = {
+  HomeScreen: undefined;
+  Screen1: { id: number };
+  Screen2: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+     //?CONFIG
+     /* initialRouteName='HomeScreen'
+     screenOptions={{
+      headerShown: false,
+      cardStyle: {
+        backgroundColor: 'white'
+      },
+     }} */
+    >
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
+    </Stack.Navigator>
+  );
+}
